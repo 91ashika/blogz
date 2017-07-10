6abdd5ea1c62bd0cbe96d5492453260b49ba2221
Ashika <91ashika@gmail.com>
2017-07-09 22:09:19 -0700

    Added links to title to redirect to each blog and form validation

--- main.py
+++ main.py
@@ -20,49 +20,39 @@ class Blog(db.Model):
 
 
 @app.route('/blog', methods=['GET'])
-def blog():
-    if len(request.args) != 0:
+def blog(): 
+    if len(request.args) != 0:      #check if there is any arguments in the request
         id = request.args.get('id')
 
-        blog = Blog.query.get(int(id))
+        blog = Blog.query.get(id)
 
-        return render_template('blog.html',blogs=blog, mainpage=False)
-     
+        return render_template('blog.html',blogs=blog, mainpage=False) #mainpage variable to know what format the page should be displayed     
         
-    blogs = Blog.query.all()
+    blogs = Blog.query.all()  # if the blog mainpage is loaded, display all the blogs from the db
     
     return render_template('blog.html',blogs=blogs,mainpage=True)
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
-    if request.method == 'POST':
+    if request.method == 'POST':    # add the blog to the database after submit 
+        
         title = request.form['title']
         blog = request.form['body']
+        if not title or not blog:   #validate the fields
+            error = "Fill both the fields."
+            return render_template("newpost.html",error=error)
         new_blog = Blog(title,blog)
         db.session.add(new_blog)
         db.session.commit()
 
-        blogs = Blog.query.filter_by(title=title).first()
-        id = str(blogs.id)
+        #blogs = Blog.query.filter_by(title=title).first()
+        id = str(new_blog.id) # get id from the object after commit.Id is populated only when commited.
         
-        return redirect('/blog?id='+id)
+        return redirect('/blog?id='+id)    #redirect to blog page with id.
 
-    return render_template('newpost.html')
+    return render_template('newpost.html')  # render newpost template if the request is GET
 
 
-
-'''@app.route('/delete-task', methods=['POST'])
-def delete_task():
-
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
-    db.session.commit()
-
-    return redirect('/')
-
-'''
 @app.route('/', methods=['POST', 'GET'])
 def index():
     return redirect("/blog")
